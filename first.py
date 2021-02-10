469aa836cf5ff8a82ca81fceca4cde9260371e42
Your Name <you@example.com>
2021-02-10 20:53:16 +0500

    first commit

---
+++ first.py
@@ -0,0 +1,26 @@
+print('''odami ersang,demagil odami,
+oniki,yo'q xalq g‘amidan gami''')
+print("""Ya Alloh bilambman hayot qadrini,
+shuncha yashadim lek mazmunsiz kunim.
+Gunohkor bilar ekan gunoh qadrini,
+Qancha yo‘l bosib o‘tdim tavbasiz  tunim""")
+print('Ko‘zlaring shunchaki chiroyli emas,\nQo\'yilgan sehirli tumor.\nSoniya,bir nafas,bir lahza,hullas')
+
+
+print(4-8/2)
+print(12*2-22)
+print(30/7)
+print(28//5)
+print(9**2)
+print(9*6-17//3+2**3)
+print("\"Nexia\",\"Tico\",'Damas' ko'rganlar qilar havas")
+print(5**4)
+print(22%4)
+print("Berilgan kvadratning yuzi",125**2,"perimetri",4*125,)
+print("Berilgan doiraning yuzi",3.14*6**2,"shunga teng")
+print("Berilgan uchburchakning gipotenuzasi",(6**2+7**2)**(1/2),"ga teng")
+
+
+
+
+
